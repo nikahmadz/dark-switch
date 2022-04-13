a33d34d61e2765c49af8d0b0fd84cd98bf8f97e1
Nik <7868782+nikahmadz@users.noreply.github.com>
2022-04-13 13:07:23 +0800

    fix script

--- assets/js/switch-v0.0.1.js
+++ assets/js/switch-v0.0.1.js
@@ -1,5 +1,5 @@
 ((w,i)=>{
-a=document.getElementsByName('color-scheme');
-if(a)e=a[0];
-if(e){b=e.content;s=['dark','light'];x=(b==s[0])?1:0;w[i]=()=>{e.content=s[x++]);if(x>s.length-1)x=0;}}
+a=document.getElementsByName('color-scheme')
+if(a)e=a[0]
+if(e){b=e.content;s=['dark','light'];x=(b==s[0])?1:0;w[i]=()=>{e.content=s[x++];if(x>s.length-1){x=0}}}
 })(window,'flickSwitch')
