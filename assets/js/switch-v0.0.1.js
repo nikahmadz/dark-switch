9efbfa7d724c2404d5d10a26649843e097b3ac57
Nik <7868782+nikahmadz@users.noreply.github.com>
2022-04-13 15:19:05 +0800

    update script

--- assets/js/switch-v0.0.1.js
+++ assets/js/switch-v0.0.1.js
@@ -1,13 +1,16 @@
-((w,i)=>{
+/* (c) Copyright : github.com/nikahmadz  */
+(()=>{
 d=document;
 n='color-scheme';
 a=d.getElementsByName(n);
 if(a){e=a[0]}else{e=d.createElement('meta');e.name=n}
-b=e.content='dark';s=[b,'light'];
-x=(b==s[0])?1:0;w[i]=()=>{e.content=s[x++];if(x>s.length-1){x=0}}
+b=e.content='dark';s=[b,'light'];x=(b==s[0])?1:0;
+window.flickSwitch=()=>{
+e.content=s[x++];if(x>s.length-1){x=0}
 d.querySelectorAll('html,body *').forEach(
 e=>e.style=(e.getAttribute('style')||'')
 +';background:unset;color:'
-+(/^(a|button)$/i.test(e.tagName)?'#47b':'initial')
++(/^(a$|ab|bu)/i.test(e.tagName)?'#47b':'initial')
 )
-})(window,'flickSwitch')
+}
+})()
