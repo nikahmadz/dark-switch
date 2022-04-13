9a65bbe5811deb0a92aaed514ea5f3b4e55676ca
Nik <7868782+nikahmadz@users.noreply.github.com>
2022-04-13 15:43:53 +0800

    update script

--- assets/js/switch-v0.0.1.js
+++ assets/js/switch-v0.0.1.js
@@ -4,8 +4,10 @@ d=document;
 n='color-scheme';
 a=d.getElementsByName(n);
 if(a){e=a[0]}else{e=d.createElement('meta');e.name=n}
-b=e.content='dark';s=[b,'light'];x=(b==s[0])?1:0;
-window.flickSwitch=()=>{
+w=window;m=w.matchMedia;o='dark',p='light';
+b=e.content=(m&&m('(prefers-color-scheme:dark)').matches)?o:p;
+s=[o,p];x=(b==o)?1:0;
+w.darkswitch=()=>{
 e.content=s[x++];if(x>s.length-1){x=0}
 d.querySelectorAll('html,body *').forEach(
 e=>e.style=(e.getAttribute('style')||'')
