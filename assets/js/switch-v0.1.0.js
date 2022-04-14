1675d8463de64b2d643adb2606968d358e1a0ff4
Nik <7868782+nikahmadz@users.noreply.github.com>
2022-04-14 11:02:10 +0800

    update script

--- assets/js/switch-v0.1.0.js
+++ assets/js/switch-v0.1.0.js
@@ -1,5 +1,5 @@
 /* (c) Copyright : github.com/nikahmadz  */
-(darkswitch=()=>{
+darkswitch=()=>{
 a=['light','dark'];d=document;
 d.querySelectorAll('html,body,body *:not(noscript,script,style,link,hr,br,mark,input,select,meter,progress)')
 .forEach(e=>{t=e.tagName;s='initial';
@@ -10,4 +10,4 @@ e=d.getElementsByName(n='color-scheme')[0];m=matchMedia;
 if(!e){e=d.createElement('meta');e.name=n;d.head.appendChild(e)}
 s=a.indexOf(c=e.content);x=s<0?c?m&&m('(prefers-'+n+':dark)').matches?1:0:0:s;
 e.content=a[x=++x>1?0:x];
-})()
+}
